990b319ba35f495f39928535824048d0ceb3ee1d
pstrong99 <pstrong99@gmail.com>
2010-10-31 18:53:22 +0000

    MVC documentation

---  dark-matter-data/dark-matter-data/src/org/dmd/features/extgwt/client/extended/ServerEventController.java
+++  dark-matter-data/dark-matter-data/src/org/dmd/features/extgwt/client/extended/ServerEventController.java
@@ -3,19 +3,30 @@ package org.dmd.features.extgwt.client.extended;
 import java.util.ArrayList;
 import java.util.HashMap;
 
+import org.dmd.dmp.shared.domains.DarkMatterEventsDomain;
+import org.dmd.dmp.shared.generated.dmo.EventDMO;
 import org.dmd.features.extgwt.client.ServerEventHandlerIF;
 import org.dmd.features.extgwt.client.generated.mvc.ServerEventControllerMVC;
 
 import com.extjs.gxt.ui.client.mvc.AppEvent;
 
+import de.novanic.eventservice.client.event.Event;
+import de.novanic.eventservice.client.event.RemoteEventService;
+import de.novanic.eventservice.client.event.RemoteEventServiceFactory;
+import de.novanic.eventservice.client.event.listener.RemoteEventListener;
+
 /**
- * The DMMVCEventController provides registration with the GWT Event Services
- * for your application. When an MvcEvent is specified as having a serverEventClass
- * attribute, the MVC framework automatically generates code to register the 
- * component with the event controller. Events for objects of the specified class
+ * The ServerEventController provides registration with the GWT Event Services
+ * for your application. When you indicate that your controller/view handles an 
+ * MvcServerEvent, the MVC framework automatically generates code to register the 
+ * component with the server event controller. Events for objects of the specified class
  * are then forwarded to the component automatically.
  */
 public class ServerEventController extends ServerEventControllerMVC {
+	
+	// Our handle to the event service
+	RemoteEventService eventService;
+
 	
 	// Key: The class name in which a handler is interested
 	// Value: The event handler
@@ -24,8 +35,39 @@ public class ServerEventController extends ServerEventControllerMVC {
 	@Override
 	protected void initialize() {
 		handlers = new HashMap<String, ArrayList<ServerEventHandlerIF>>();
+		
+	    eventService = RemoteEventServiceFactory.getInstance().getRemoteEventService();
 	}
 
+	/**
+	 * When the MVC framework is initialized, we attempt to add a listener to 
+	 * the Dark Matter Event domain.
+	 */
+	@Override
+	protected void handleMvcInitEvent(AppEvent event) {
+		
+	    eventService.addListener(DarkMatterEventsDomain.DOMAIN, new RemoteEventListener() {
+	        public void apply(Event anEvent) {
+	            if(anEvent instanceof EventDMO) {
+	            	EventDMO event = (EventDMO) anEvent;
+	            	
+	            	ArrayList<ServerEventHandlerIF> interested = handlers.get(event.getObjClass());
+	            	if (interested != null){
+	            		for(ServerEventHandlerIF h : interested){
+	            			h.handleServerEvent(event);
+	            		}
+	            	}
+	            }
+	        }
+	    });
+
+		registerMvcServerEventController(this);
+		
+		// Sending this event will cause all controllers and views that want server events
+		// to add themselves as event handlers.
+		dispatchMvcInitEventFramework();
+	}
+	
 	/**
 	 * Adds a handler for events associated with the specified object class.
 	 * @param handler  The event handler.
@@ -41,15 +83,10 @@ public class ServerEventController extends ServerEventControllerMVC {
 		
 		listeners.add(handler);
 	}
-
-	@Override
-	protected void handleMvcInitEvent(AppEvent event) {
-		registerMvcServerEventController(this);
+	
+	void dispatchServerEvent(EventDMO event){
 		
-		
-		
-		// Sending this event will cause all controllers and views that want server events
-		// to add themselves as event handlers.
-		dispatchMvcInitEventFramework();
 	}
+
+
 }
