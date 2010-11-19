a05eb6b2c3a9f82cc650a821f78635c52c448110
pstrong99 <pstrong99@gmail.com>
2010-11-19 14:59:13 +0000

    MVC menus

---  dark-matter-data/dark-matter-data/src/org/dmd/features/extgwt/extended/MvcPopupMenu.java
+++  dark-matter-data/dark-matter-data/src/org/dmd/features/extgwt/extended/MvcPopupMenu.java
@@ -4,4 +4,24 @@ import org.dmd.features.extgwt.generated.dmw.MvcPopupMenuDMW;
 
 public class MvcPopupMenu extends MvcPopupMenuDMW {
 
+	@Override
+	public String getDefaultImport(){
+		return("org.dmd.features.extgwt.client.util.menu.DefaultPopupMenu");
+	}
+	
+	@Override
+	public String getDefaultClass(){
+		return("DefaultPopupMenu");
+	}
+
+	@Override
+	public String getInstantiation(){
+		if (getCustomRender()){
+			return(null);
+		}
+		else{
+			return("        " + getVariableName() + " = new " + getDefaultClass() + "(\"" + getName() + "\");\n");
+		}
+	}
+
 }
