8da0265d9af3e8b68cdc7030959dc2db21b28bf2
pstrong99 <pstrong99@gmail.com>
2011-04-15 04:39:52 +0000

    Addition of DmcNameBuilderIf support to the ASAGs and DmcOmni

---  dark-matter-data/dark-matter-data/src/org/dmd/dmt/server/generated/dmw/DmtStringNameIterableDMW.java
+++  dark-matter-data/dark-matter-data/src/org/dmd/dmt/server/generated/dmw/DmtStringNameIterableDMW.java
@@ -10,7 +10,7 @@ import org.dmd.dmt.shared.types.DmtStringName;
  * <P>
  * This code was auto-generated and shouldn't be altered manually!
  * Generated from: org.dmd.dms.util.GenUtility.dumpIterable(GenUtility.java:1165)
- *    Called from: org.dmd.dmg.generators.DMWGenerator.createTypeIterables(DMWGenerator.java:158)
+ *    Called from: org.dmd.dmg.generators.DMWGenerator.createTypeIterables(DMWGenerator.java:162)
  */
 public class DmtStringNameIterableDMW extends DmwMVIterator<DmtStringName> {
 
---  dark-matter-data/dark-matter-data/src/org/dmd/dmt/server/generated/dmw/TestAbstractFixedIterableDMW.java
+++  dark-matter-data/dark-matter-data/src/org/dmd/dmt/server/generated/dmw/TestAbstractFixedIterableDMW.java
@@ -3,14 +3,15 @@ package org.dmd.dmt.server.generated.dmw;
 import java.util.Iterator;
 
 import org.dmd.dmw.DmwObjectIterator;
-import org.dmd.dmt.shared.generated.dmo.TestAbstractFixedDMO;
+import org.dmd.dmt.shared.generated.dmo.TestAbstractFixedDMO;  // dmo type import
+import org.dmd.dmt.server.generated.dmw.TestAbstractFixedDMW;  // wrapper type import
 /**
  * The TestAbstractFixedIterableDMW wraps an Iterator for a particular type and makes 
  * it Iterable.
  * <P>
  * This code was auto-generated and shouldn't be altered manually!
- * Generated from: org.dmd.dms.util.GenUtility.dumpObjectIterable(GenUtility.java:1216)
- *    Called from: org.dmd.dmg.generators.DMWGenerator.createTypeIterables(DMWGenerator.java:144)
+ * Generated from: org.dmd.dms.util.GenUtility.dumpObjectIterable(GenUtility.java:1219)
+ *    Called from: org.dmd.dmg.generators.DMWGenerator.createTypeIterables(DMWGenerator.java:148)
  */
 public class TestAbstractFixedIterableDMW extends DmwObjectIterator<TestAbstractFixedDMW, TestAbstractFixedDMO> {
 
