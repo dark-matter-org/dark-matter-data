675af40797fbc9733019a0eec379b41a32ace3b6
pstrong99 <pstrong99@gmail.com>
2011-05-05 12:53:07 +0000

    Changing package structure

---  dark-matter-data/dark-matter-data/src/org/dmd/mvw/extended/LocalEvent.java
+++
@@ -1,7 +0,0 @@
-package org.dmd.mvw.extended;
-
-import org.dmd.mvw.generated.dmw.LocalEventDMW;
-
-public class LocalEvent extends LocalEventDMW {
-
-}
---  dark-matter-data/dark-matter-data/src/org/dmd/mvw/extended/MvwConfig.java
+++
@@ -1,7 +0,0 @@
-package org.dmd.mvw.extended;
-
-import org.dmd.mvw.generated.dmw.MvwConfigDMW;
-
-public class MvwConfig extends MvwConfigDMW {
-
-}
