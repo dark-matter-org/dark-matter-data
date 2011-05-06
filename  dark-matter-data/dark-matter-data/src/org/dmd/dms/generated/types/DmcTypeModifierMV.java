6467aaa7b7719e5a67b9ccfaee4c38f1a5f05778
pstrong99 <pstrong99@gmail.com>
2011-05-06 02:15:15 +0000

    Complex type support in the meta schema

---  dark-matter-data/dark-matter-data/src/org/dmd/dms/generated/types/DmcTypeModifierMV.java
+++  dark-matter-data/dark-matter-data/src/org/dmd/dms/generated/types/DmcTypeModifierMV.java
@@ -27,8 +27,8 @@ import org.dmd.dmc.types.DmcTypeModifier;    // DmcType import
  * The DmcTypeModifierMV provides storage for a multi-valued Modifier
  * <P>
  * This code was auto-generated and shouldn't be altered manually!
- * Generated from:  org.dmd.dms.util.GenUtility.dumpMVType(GenUtility.java:1841)
- *    Called from:  org.dmd.dms.meta.MetaGenerator.dumpDerivedTypes(MetaGenerator.java:253)
+ * Generated from: org.dmd.dms.util.GenUtility.dumpMVType(GenUtility.java:1841)
+ *    Called from: org.dmd.dms.meta.MetaGenerator.dumpDerivedTypes(MetaGenerator.java:254)
  */
 @SuppressWarnings("serial")
 public class DmcTypeModifierMV extends DmcTypeModifier implements Serializable {
