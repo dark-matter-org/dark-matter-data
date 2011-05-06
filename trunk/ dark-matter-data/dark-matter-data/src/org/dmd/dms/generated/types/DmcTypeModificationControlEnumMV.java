61d6386a8000cc75670c72acb6197012fe9f1e81
pstrong99@gmail.com <pstrong99@gmail.com@180258a6-fd51-281c-9bda-22f77bc2175c>
2011-05-06 02:15:15 +0000

    Complex type support in the meta schema

--- trunk/ dark-matter-data/dark-matter-data/src/org/dmd/dms/generated/types/DmcTypeModificationControlEnumMV.java
+++ trunk/ dark-matter-data/dark-matter-data/src/org/dmd/dms/generated/types/DmcTypeModificationControlEnumMV.java
@@ -26,8 +26,8 @@ import org.dmd.dms.generated.enums.ModificationControlEnum;    // DmcType import
  * The DmcTypeModificationControlEnumMV provides storage for a multi-valued ModificationControlEnum
  * <P>
  * This code was auto-generated and shouldn't be altered manually!
- * Generated from:  org.dmd.dms.util.GenUtility.dumpMVType(GenUtility.java:1841)
- *    Called from:  org.dmd.dms.meta.MetaGenerator.dumpDerivedTypes(MetaGenerator.java:222)
+ * Generated from: org.dmd.dms.util.GenUtility.dumpMVType(GenUtility.java:1841)
+ *    Called from: org.dmd.dms.meta.MetaGenerator.dumpDerivedTypes(MetaGenerator.java:223)
  */
 @SuppressWarnings("serial")
 public class DmcTypeModificationControlEnumMV extends DmcTypeModificationControlEnum implements Serializable {
