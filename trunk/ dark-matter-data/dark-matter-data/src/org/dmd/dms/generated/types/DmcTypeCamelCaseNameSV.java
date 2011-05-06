56bee00508db2ac9b473649609130e2ca46b9acd
pstrong99@gmail.com <pstrong99@gmail.com@180258a6-fd51-281c-9bda-22f77bc2175c>
2011-05-06 02:12:30 +0000

    Complex type support in the meta schema

--- trunk/ dark-matter-data/dark-matter-data/src/org/dmd/dms/generated/types/DmcTypeCamelCaseNameSV.java
+++ trunk/ dark-matter-data/dark-matter-data/src/org/dmd/dms/generated/types/DmcTypeCamelCaseNameSV.java
@@ -25,8 +25,8 @@ import org.dmd.dmc.types.DmcTypeCamelCaseName;    // DmcType import
  * The DmcTypeCamelCaseNameSV provides storage for a single-valued CamelCaseName
  * <P>
  * This code was auto-generated and shouldn't be altered manually!
- * Generated from:  org.dmd.dms.util.GenUtility.dumpSVType(GenUtility.java:1546)
- *    Called from:  org.dmd.dms.meta.MetaGenerator.dumpDerivedTypes(MetaGenerator.java:252)
+ * Generated from: org.dmd.dms.util.GenUtility.dumpSVType(GenUtility.java:1546)
+ *    Called from: org.dmd.dms.meta.MetaGenerator.dumpDerivedTypes(MetaGenerator.java:253)
  */
 @SuppressWarnings("serial")
 public class DmcTypeCamelCaseNameSV extends DmcTypeCamelCaseName implements Serializable {
