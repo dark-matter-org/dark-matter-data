90a689b88b42d4b490afd6dc25bb0fabb2f57c54
pstrong99 <pstrong99@gmail.com>
2011-05-06 02:14:44 +0000

    Complex type support in the meta schema

---  dark-matter-data/dark-matter-data/src/org/dmd/dms/generated/types/DmcTypeIntegerToBooleanSET.java
+++  dark-matter-data/dark-matter-data/src/org/dmd/dms/generated/types/DmcTypeIntegerToBooleanSET.java
@@ -30,8 +30,8 @@ import org.dmd.dmc.types.DmcTypeIntegerToBoolean;    // DmcType import
  * The DmcTypeIntegerToBooleanSET provides storage for a set of IntegerToBoolean
  * <P>
  * This code was auto-generated and shouldn't be altered manually!
- * Generated from:  org.dmd.dms.util.GenUtility.dumpSETType(GenUtility.java:2034)
- *    Called from:  org.dmd.dms.meta.MetaGenerator.dumpDerivedTypes(MetaGenerator.java:254)
+ * Generated from: org.dmd.dms.util.GenUtility.dumpSETType(GenUtility.java:2034)
+ *    Called from: org.dmd.dms.meta.MetaGenerator.dumpDerivedTypes(MetaGenerator.java:255)
  */
 @SuppressWarnings("serial")
 public class DmcTypeIntegerToBooleanSET extends DmcTypeIntegerToBoolean implements Serializable {
