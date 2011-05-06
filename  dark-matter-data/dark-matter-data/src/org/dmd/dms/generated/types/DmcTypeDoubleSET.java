cfcc432685a0e235408db6c7b2a8b37d1b3373ec
pstrong99 <pstrong99@gmail.com>
2011-05-06 02:14:06 +0000

    Complex type support in the meta schema

---  dark-matter-data/dark-matter-data/src/org/dmd/dms/generated/types/DmcTypeDoubleSET.java
+++  dark-matter-data/dark-matter-data/src/org/dmd/dms/generated/types/DmcTypeDoubleSET.java
@@ -29,8 +29,8 @@ import org.dmd.dmc.types.DmcTypeDouble;    // DmcType import
  * The DmcTypeDoubleSET provides storage for a set of Double
  * <P>
  * This code was auto-generated and shouldn't be altered manually!
- * Generated from:  org.dmd.dms.util.GenUtility.dumpSETType(GenUtility.java:2034)
- *    Called from:  org.dmd.dms.meta.MetaGenerator.dumpDerivedTypes(MetaGenerator.java:254)
+ * Generated from: org.dmd.dms.util.GenUtility.dumpSETType(GenUtility.java:2034)
+ *    Called from: org.dmd.dms.meta.MetaGenerator.dumpDerivedTypes(MetaGenerator.java:255)
  */
 @SuppressWarnings("serial")
 public class DmcTypeDoubleSET extends DmcTypeDouble implements Serializable {
---  dark-matter-data/dark-matter-data/src/org/dmd/dms/generated/types/DmcTypeFieldSET.java
+++  dark-matter-data/dark-matter-data/src/org/dmd/dms/generated/types/DmcTypeFieldSET.java
@@ -30,8 +30,8 @@ import org.dmd.dms.generated.types.DmcTypeField;    // DmcType import
  * The DmcTypeFieldSET provides storage for a set of Field
  * <P>
  * This code was auto-generated and shouldn't be altered manually!
- * Generated from:  org.dmd.dms.util.GenUtility.dumpSETType(GenUtility.java:2034)
- *    Called from:  org.dmd.dms.meta.MetaGenerator.dumpDerivedTypes(MetaGenerator.java:254)
+ * Generated from: org.dmd.dms.util.GenUtility.dumpSETType(GenUtility.java:2034)
+ *    Called from: org.dmd.dms.meta.MetaGenerator.dumpDerivedTypes(MetaGenerator.java:255)
  */
 @SuppressWarnings("serial")
 public class DmcTypeFieldSET extends DmcTypeField implements Serializable {
