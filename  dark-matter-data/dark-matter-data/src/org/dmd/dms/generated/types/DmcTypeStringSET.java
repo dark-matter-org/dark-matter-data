e5f3d6bf1d4a4d29cd28a5b5c0c4804cc07a68af
pstrong99 <pstrong99@gmail.com>
2011-05-06 02:16:16 +0000

    Complex type support in the meta schema

---  dark-matter-data/dark-matter-data/src/org/dmd/dms/generated/types/DmcTypeStringSET.java
+++  dark-matter-data/dark-matter-data/src/org/dmd/dms/generated/types/DmcTypeStringSET.java
@@ -29,8 +29,8 @@ import org.dmd.dmc.types.DmcTypeString;    // DmcType import
  * The DmcTypeStringSET provides storage for a set of String
  * <P>
  * This code was auto-generated and shouldn't be altered manually!
- * Generated from:  org.dmd.dms.util.GenUtility.dumpSETType(GenUtility.java:2034)
- *    Called from:  org.dmd.dms.meta.MetaGenerator.dumpDerivedTypes(MetaGenerator.java:254)
+ * Generated from: org.dmd.dms.util.GenUtility.dumpSETType(GenUtility.java:2034)
+ *    Called from: org.dmd.dms.meta.MetaGenerator.dumpDerivedTypes(MetaGenerator.java:255)
  */
 @SuppressWarnings("serial")
 public class DmcTypeStringSET extends DmcTypeString implements Serializable {
---  dark-matter-data/dark-matter-data/src/org/dmd/dms/generated/types/DmcTypeWrapperTypeEnumSV.java
+++  dark-matter-data/dark-matter-data/src/org/dmd/dms/generated/types/DmcTypeWrapperTypeEnumSV.java
@@ -24,8 +24,8 @@ import org.dmd.dms.generated.enums.WrapperTypeEnum;    // DmcType import
  * The DmcTypeWrapperTypeEnumSV provides storage for a single-valued WrapperTypeEnum
  * <P>
  * This code was auto-generated and shouldn't be altered manually!
- * Generated from:  org.dmd.dms.util.GenUtility.dumpSVType(GenUtility.java:1546)
- *    Called from:  org.dmd.dms.meta.MetaGenerator.dumpDerivedTypes(MetaGenerator.java:221)
+ * Generated from: org.dmd.dms.util.GenUtility.dumpSVType(GenUtility.java:1546)
+ *    Called from: org.dmd.dms.meta.MetaGenerator.dumpDerivedTypes(MetaGenerator.java:222)
  */
 @SuppressWarnings("serial")
 public class DmcTypeWrapperTypeEnumSV extends DmcTypeWrapperTypeEnum implements Serializable {
