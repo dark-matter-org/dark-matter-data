3fcf91794af097b92f68ecc702b875dc3b346b35
pstrong99 <pstrong99@180258a6-fd51-281c-9bda-22f77bc2175c>
2011-05-16 13:18:26 +0000

    Further work on MVW

---  dark-matter-data/dark-matter-data/src/org/dmd/dmp/server/EventHandlerIF.java
+++  dark-matter-data/dark-matter-data/src/org/dmd/dmp/server/EventHandlerIF.java
@@ -1,9 +1,14 @@
 package org.dmd.dmp.server;
 
+import org.dmd.dmp.server.extended.DMPEvent;
+
 /**
  * The EventHandlerIF interface is implemented by entities that want to register
  * with an EventBusIF for DMPEvent notification.
  */
 public interface EventHandlerIF {
+	
+	public InitiatorID getInitiatorID();
 
+	public void handleEvent(DMPEvent event);
 }
