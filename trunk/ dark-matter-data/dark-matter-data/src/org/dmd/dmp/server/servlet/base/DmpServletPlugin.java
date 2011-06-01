de208f81e4173ea726585f819766fac1efaba041
pstrong99@gmail.com <pstrong99@gmail.com@180258a6-fd51-281c-9bda-22f77bc2175c>
2011-06-01 15:34:29 +0000

    Servlet reference implementation

--- trunk/ dark-matter-data/dark-matter-data/src/org/dmd/dmp/server/servlet/base/DmpServletPlugin.java
+++ trunk/ dark-matter-data/dark-matter-data/src/org/dmd/dmp/server/servlet/base/DmpServletPlugin.java
@@ -1,5 +1,6 @@
 package org.dmd.dmp.server.servlet.base;
 
+import org.dmd.dmp.server.servlet.base.interfaces.CacheIF;
 import org.dmd.util.exceptions.ResultException;
 
 /**
@@ -11,6 +12,12 @@ public class DmpServletPlugin {
 	static int nextID;
 	
 	int ID;
+	
+	// Handles to the overall plugin manager and the cache. These are set before
+	// the plugin's init() method is called. If the plugin requires access to 
+	// other plugin's, it can access them from the PluginManager.
+	PluginManager	pluginManager;
+	CacheIF			cache;
 
 	public DmpServletPlugin(){
 		ID = nextID++;
@@ -24,12 +31,17 @@ public class DmpServletPlugin {
 		return(ID);
 	}
 	
+	protected void setManagerAndCache(PluginManager pm, CacheIF c){
+		pluginManager	= pm;
+		cache			= c;
+	}
+	
 	/**
 	 * Derived classes should perform any initialization logic required. If a
 	 * plugin has indicated that it depends on another plugin, it is safe to 
 	 * assume that the other plugin has been initialized.
 	 */
-	void init() throws ResultException {
+	protected void init() throws ResultException {
 		
 	}
 	
@@ -37,14 +49,14 @@ public class DmpServletPlugin {
 	 * After all plugins are initialized, the start method is called on all plugins
 	 * in the same implied order.
 	 */
-	void start(){
+	protected void start(){
 		
 	}
 	
 	/**
 	 * When the servlet is shutdown, the destroy() method is called on all plugins.
 	 */
-	void destroy(){
+	protected void destroy(){
 		
 	}
 }
