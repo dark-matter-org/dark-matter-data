2e4cbe4255a95fe7f64d0d23b0311569232bb157
pstrong99 <pstrong99@gmail.com>
2011-06-06 21:10:20 +0000

    Change over to DmwWrapper, DmwNamedObjectWrapper and DmwHierarchObjectWrapper classes for wrappers.

---  dark-matter-data/dark-matter-data/src/org/dmd/dmr/shared/ldap/generated/types/DmcTypeLDAPHierarchicObjectREFMAP.java
+++  dark-matter-data/dark-matter-data/src/org/dmd/dmr/shared/ldap/generated/types/DmcTypeLDAPHierarchicObjectREFMAP.java
@@ -30,8 +30,8 @@ import org.dmd.dmc.types.FullyQualifiedName;    // key type import
  * The DmcTypeLDAPHierarchicObjectREFMAP provides storage for a map of LDAPHierarchicObjectREF
  * <P>
  * This code was auto-generated and shouldn't be altered manually!
- * Generated from: org.dmd.dms.util.GenUtility.dumpMAPType(GenUtility.java:2239)
- *    Called from: org.dmd.dms.util.DmoTypeFormatter.dumpNamedREF(DmoTypeFormatter.java:493)
+ * Generated from:  org.dmd.dms.util.GenUtility.dumpMAPType(GenUtility.java:2240)
+ *    Called from:  org.dmd.dms.util.DmoTypeFormatter.dumpNamedREF(DmoTypeFormatter.java:498)
  */
 @SuppressWarnings("serial")
 // public class DmcTypeLDAPHierarchicObjectREFMAP extends DmcTypeLDAPHierarchicObjectREF<LDAPHierarchicObjectREF,FullyQualifiedName> {
