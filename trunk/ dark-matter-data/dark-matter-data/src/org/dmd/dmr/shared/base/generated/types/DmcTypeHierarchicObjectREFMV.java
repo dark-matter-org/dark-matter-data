04f123db5b2aba3d84ec8b93546e7f748207bec7
pstrong99@gmail.com <pstrong99@gmail.com@180258a6-fd51-281c-9bda-22f77bc2175c>
2011-06-06 21:10:20 +0000

    Change over to DmwWrapper, DmwNamedObjectWrapper and DmwHierarchObjectWrapper classes for wrappers.

--- trunk/ dark-matter-data/dark-matter-data/src/org/dmd/dmr/shared/base/generated/types/DmcTypeHierarchicObjectREFMV.java
+++ trunk/ dark-matter-data/dark-matter-data/src/org/dmd/dmr/shared/base/generated/types/DmcTypeHierarchicObjectREFMV.java
@@ -25,8 +25,8 @@ import org.dmd.dmc.DmcValueException;
  * The DmcTypeHierarchicObjectREFMV provides storage for a multi-valued HierarchicObjectREF
  * <P>
  * This code was auto-generated and shouldn't be altered manually!
- * Generated from: org.dmd.dms.util.GenUtility.dumpMVType(GenUtility.java:1841)
- *    Called from: org.dmd.dms.util.DmoTypeFormatter.dumpNamedREF(DmoTypeFormatter.java:484)
+ * Generated from:  org.dmd.dms.util.GenUtility.dumpMVType(GenUtility.java:1842)
+ *    Called from:  org.dmd.dms.util.DmoTypeFormatter.dumpNamedREF(DmoTypeFormatter.java:489)
  */
 @SuppressWarnings("serial")
 public class DmcTypeHierarchicObjectREFMV extends DmcTypeHierarchicObjectREF implements Serializable {
