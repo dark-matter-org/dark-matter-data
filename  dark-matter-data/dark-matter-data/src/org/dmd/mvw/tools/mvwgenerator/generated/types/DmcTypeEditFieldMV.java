377ff6c47dd5d5c8d0f31574c0d7132f1f6b52e7
pstrong99 <pstrong99@gmail.com>
2011-07-25 00:47:31 +0000

    Starting work on forms and field binding

---
+++  dark-matter-data/dark-matter-data/src/org/dmd/mvw/tools/mvwgenerator/generated/types/DmcTypeEditFieldMV.java
@@ -0,0 +1,206 @@
+//	---------------------------------------------------------------------------
+//	dark-matter-data
+//	Copyright (c) 2011 dark-matter-data committers
+//	---------------------------------------------------------------------------
+//	This program is free software; you can redistribute it and/or modify it
+//	under the terms of the GNU Lesser General Public License as published by the
+//	Free Software Foundation; either version 3 of the License, or (at your
+//	option) any later version.
+//	This program is distributed in the hope that it will be useful, but WITHOUT
+//	ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
+//	FITNESS FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for
+//	more details.
+//	You should have received a copy of the GNU Lesser General Public License along
+//	with this program; if not, see <http://www.gnu.org/licenses/lgpl.html>.
+//	---------------------------------------------------------------------------
+package org.dmd.mvw.tools.mvwgenerator.generated.types;
+
+import java.io.Serializable;
+import java.util.ArrayList;
+import java.util.Iterator;
+import org.dmd.dmc.DmcAttribute;
+import org.dmd.dmc.DmcAttributeInfo;
+import org.dmd.dmc.DmcValueException;
+import org.dmd.mvw.tools.mvwgenerator.types.EditField;    // base type import
+import org.dmd.mvw.tools.mvwgenerator.types.DmcTypeEditField;    // DmcType import
+/**
+ * The DmcTypeEditFieldMV provides storage for a multi-valued EditField
+ * <P>
+ * This code was auto-generated and shouldn't be altered manually!
+ * Generated from: org.dmd.dms.util.GenUtility.dumpMVType(GenUtility.java:2047)
+ *    Called from: org.dmd.dms.util.DmoTypeFormatter.dumpTypes(DmoTypeFormatter.java:100)
+ */
+@SuppressWarnings("serial")
+public class DmcTypeEditFieldMV extends DmcTypeEditField implements Serializable {
+    
+    ArrayList<EditField> value;
+    
+    public DmcTypeEditFieldMV(){
+    
+    }
+    
+    public DmcTypeEditFieldMV(DmcAttributeInfo ai){
+        super(ai);
+    }
+    
+    @Override
+    public DmcTypeEditFieldMV getNew(){
+        return(new DmcTypeEditFieldMV(attrInfo));
+    }
+    
+    @Override
+    // org.dmd.dms.util.GenUtility.dumpMVType(GenUtility.java:2075)
+    public DmcAttribute<EditField> cloneIt(){
+        synchronized(this){
+            DmcTypeEditFieldMV rc = getNew();
+            if (attrInfo.indexSize == 0){
+                for(EditField val: value)
+                try {
+                    rc.add(val);
+                } catch (DmcValueException e) {
+                    throw(new IllegalStateException("typeCheck() should never fail here!",e));
+                }
+            }
+            else{
+                for(int index=0; index<value.size(); index++)
+                    try {
+                        rc.setMVnth(index, value.get(index));
+                    } catch (DmcValueException e) {
+                        throw(new IllegalStateException("typeCheck() should never fail here!",e));
+                    }
+            }
+            return(rc);
+        }
+    }
+    
+    @Override
+    // org.dmd.dms.util.GenUtility.dumpMVType(GenUtility.java:2104)
+    public EditField add(Object v) throws DmcValueException {
+        synchronized(this){
+            EditField rc = typeCheck(v);
+            if (value == null)
+                value = new ArrayList<EditField>();
+            value.add(rc);
+            return(rc);
+        }
+    }
+    
+    @Override
+    // org.dmd.dms.util.GenUtility.dumpMVType(GenUtility.java:2117)
+    public EditField del(Object v){
+        synchronized(this){
+            EditField rc = null;
+            try {
+                rc = typeCheck(v);
+            } catch (DmcValueException e) {
+                throw(new IllegalStateException("Incompatible type passed to del():" + getName(),e));
+            }
+            if (value.contains(rc))
+                value.remove(rc);
+            else
+                rc = null;
+            return(rc);
+        }
+    }
+    
+    @Override
+    // org.dmd.dms.util.GenUtility.dumpMVType(GenUtility.java:2136)
+    public Iterator<EditField> getMV(){
+        synchronized(this){
+            ArrayList<EditField> clone = new ArrayList<EditField>(value);
+            return(clone.iterator());
+        }
+    }
+    
+    // org.dmd.dms.util.GenUtility.dumpMVType(GenUtility.java:2145)
+    public ArrayList<EditField> getMVCopy(){
+        synchronized(this){
+            ArrayList<EditField> clone = new ArrayList<EditField>(value);
+            return(clone);
+        }
+    }
+    
+    @Override
+    // org.dmd.dms.util.GenUtility.dumpMVType(GenUtility.java:2155)
+    public int getMVSize(){
+        synchronized(this){
+            if (value == null)
+                return(0);
+            return(value.size());
+        }
+    }
+    
+    @Override
+    // org.dmd.dms.util.GenUtility.dumpMVType(GenUtility.java:2166)
+    public EditField getMVnth(int index){
+        synchronized(this){
+            return(value.get(index));
+        }
+    }
+    
+    @Override
+    // org.dmd.dms.util.GenUtility.dumpMVType(GenUtility.java:2175)
+    public EditField setMVnth(int index, Object v) throws DmcValueException {
+        synchronized(this){
+            if (attrInfo.indexSize == 0)
+                throw(new IllegalStateException("Attribute: " + attrInfo.name + " is not indexed. You can't use setMVnth()."));
+            
+            if ( (index < 0) || (index >= attrInfo.indexSize))
+                throw(new IllegalStateException("Index " + index + " for attribute: " + attrInfo.name + " is out of range: 0 <= index < " + attrInfo.indexSize));
+            
+            EditField rc = null;
+            
+            if (v != null)
+                rc = typeCheck(v);
+            
+            if (value == null){
+                value = new ArrayList<EditField>(attrInfo.indexSize);
+                for(int i=0;i<attrInfo.indexSize;i++)
+                    value.add(null);
+            }
+            
+            value.set(index, rc);
+            
+            return(rc);
+        }
+    }
+    
+    @Override
+    // org.dmd.dms.util.GenUtility.dumpMVType(GenUtility.java:2203)
+    public boolean hasValue(){
+        synchronized(this){
+            boolean rc = false;
+            
+            if (attrInfo.indexSize == 0)
+                throw(new IllegalStateException("Attribute: " + attrInfo.name + " is not indexed. You can't use hasValue()."));
+            
+            if (value == null)
+                return(rc);
+            
+            for(int i=0; i<value.size(); i++){
+                if (value.get(i) != null){
+                    rc = true;
+                    break;
+                }
+            }
+            
+            return(rc);
+        }
+    }
+    
+    @Override
+    // org.dmd.dms.util.GenUtility.dumpMVType(GenUtility.java:2227)
+    public boolean contains(Object v){
+        synchronized(this){
+            boolean rc = false;
+            try {
+                EditField val = typeCheck(v);
+                rc = value.contains(val);
+            } catch (DmcValueException e) {
+            }
+            return(rc);
+        }
+    }
+    
+}
+
---
+++  dark-matter-data/dark-matter-data/src/org/dmd/mvw/tools/mvwgenerator/generated/types/DmcTypeFormBindingDefinitionREFMAP.java
@@ -0,0 +1,181 @@
+//	---------------------------------------------------------------------------
+//	dark-matter-data
+//	Copyright (c) 2011 dark-matter-data committers
+//	---------------------------------------------------------------------------
+//	This program is free software; you can redistribute it and/or modify it
+//	under the terms of the GNU Lesser General Public License as published by the
+//	Free Software Foundation; either version 3 of the License, or (at your
+//	option) any later version.
+//	This program is distributed in the hope that it will be useful, but WITHOUT
+//	ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
+//	FITNESS FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for
+//	more details.
+//	You should have received a copy of the GNU Lesser General Public License along
+//	with this program; if not, see <http://www.gnu.org/licenses/lgpl.html>.
+//	---------------------------------------------------------------------------
+package org.dmd.mvw.tools.mvwgenerator.generated.types;
+
+import java.io.Serializable;
+import java.util.Map;
+import java.util.HashMap;
+import java.util.TreeMap;
+import java.util.Iterator;
+import org.dmd.dmc.DmcAttribute;
+import org.dmd.dmc.DmcAttributeInfo;
+import org.dmd.dmc.DmcValueException;
+import org.dmd.dmc.DmcMappedAttributeIF;
+import org.dmd.dms.generated.enums.ValueTypeEnum;
+import org.dmd.dmc.types.CamelCaseName;    // key type import
+/**
+ * The DmcTypeFormBindingDefinitionREFMAP provides storage for a map of FormBindingDefinitionREF
+ * <P>
+ * This code was auto-generated and shouldn't be altered manually!
+ * Generated from: org.dmd.dms.util.GenUtility.dumpMAPType(GenUtility.java:2493)
+ *    Called from: org.dmd.dms.util.DmoTypeFormatter.dumpNamedREF(DmoTypeFormatter.java:503)
+ */
+@SuppressWarnings("serial")
+// public class DmcTypeFormBindingDefinitionREFMAP extends DmcTypeFormBindingDefinitionREF<FormBindingDefinitionREF,CamelCaseName> {
+public class DmcTypeFormBindingDefinitionREFMAP extends DmcTypeFormBindingDefinitionREF implements Serializable {
+    
+    Map<CamelCaseName,FormBindingDefinitionREF> value;
+    
+    public DmcTypeFormBindingDefinitionREFMAP(){
+        value = null;
+    }
+    
+    public DmcTypeFormBindingDefinitionREFMAP(DmcAttributeInfo ai){
+        super(ai);
+        initValue();
+    }
+    
+    void initValue(){
+        if (attrInfo.valueType == ValueTypeEnum.HASHMAPPED)
+            value = new HashMap<CamelCaseName,FormBindingDefinitionREF>();
+        else
+            value = new TreeMap<CamelCaseName,FormBindingDefinitionREF>();
+    }
+    
+    @Override
+    public DmcTypeFormBindingDefinitionREFMAP getNew(){
+        return(new DmcTypeFormBindingDefinitionREFMAP(attrInfo));
+    }
+    
+    @Override
+    // org.dmd.dms.util.GenUtility.dumpMAPType(GenUtility.java:2536)
+    public DmcAttribute<FormBindingDefinitionREF> cloneIt(){
+        synchronized(this){
+            DmcTypeFormBindingDefinitionREFMAP rc = getNew();
+            for(FormBindingDefinitionREF val: value.values())
+            try {
+                rc.add(val);
+            } catch (DmcValueException e) {
+                throw(new IllegalStateException("typeCheck() should never fail here!",e));
+            }
+            return(rc);
+        }
+    }
+    
+    @Override
+    // org.dmd.dms.util.GenUtility.dumpMAPType(GenUtility.java:2552)
+    public FormBindingDefinitionREF add(Object v) throws DmcValueException {
+        synchronized(this){
+            FormBindingDefinitionREF newval = typeCheck(v);
+            if (value == null)
+                initValue();
+            CamelCaseName key = (CamelCaseName)((DmcMappedAttributeIF)newval).getKey();
+            FormBindingDefinitionREF oldval = value.put(key,newval);
+            
+            if (oldval != null){
+                // We had a value with this key, ensure that the value actually changed
+                if (oldval.valuesAreEqual(newval))
+                    newval = null;
+            }
+            
+            return(newval);
+        }
+    }
+    
+    @Override
+    // org.dmd.dms.util.GenUtility.dumpMAPType(GenUtility.java:2573)
+    public FormBindingDefinitionREF del(Object key){
+        synchronized(this){
+           if (key instanceof CamelCaseName)
+                return(value.remove(key));
+            else
+                throw(new IllegalStateException("Incompatible key type: " + key.getClass().getName() + " passed to del():" + getName()));
+        }
+    }
+    
+    @Override
+    // org.dmd.dms.util.GenUtility.dumpMAPType(GenUtility.java:2585)
+    public Iterator<FormBindingDefinitionREF> getMV(){
+        synchronized(this){
+            Map<CamelCaseName,FormBindingDefinitionREF> clone = null;
+            if (attrInfo.valueType == ValueTypeEnum.HASHMAPPED)
+                clone = new HashMap<CamelCaseName,FormBindingDefinitionREF>(value);
+            else
+                clone = new TreeMap<CamelCaseName,FormBindingDefinitionREF>(value);
+            return(clone.values().iterator());
+        }
+    }
+    
+    // org.dmd.dms.util.GenUtility.dumpMAPType(GenUtility.java:2598)
+    public Map<CamelCaseName,FormBindingDefinitionREF> getMVCopy(){
+        synchronized(this){
+            Map<CamelCaseName,FormBindingDefinitionREF> clone = null;
+            if (attrInfo.valueType == ValueTypeEnum.HASHMAPPED)
+                clone = new HashMap<CamelCaseName,FormBindingDefinitionREF>(value);
+            else
+                clone = new TreeMap<CamelCaseName,FormBindingDefinitionREF>(value);
+            return(clone);
+        }
+    }
+    
+    // org.dmd.dms.util.GenUtility.dumpMAPType(GenUtility.java:2611)
+    @Override
+    public int getMVSize(){
+        synchronized(this){
+            if (value == null)
+                return(0);
+            return(value.size());
+        }
+    }
+    
+    @Override
+    // org.dmd.dms.util.GenUtility.dumpMAPType(GenUtility.java:2623)
+    public FormBindingDefinitionREF getByKey(Object key){
+        synchronized(this){
+            if (key instanceof CamelCaseName)
+                return(value.get(key));
+            else
+                throw(new IllegalStateException("Incompatible type: " + key.getClass().getName() + " passed to del():" + getName()));
+        }
+    }
+    
+    @Override
+    // org.dmd.dms.util.GenUtility.dumpMAPType(GenUtility.java:2635)
+    public boolean contains(Object v){
+        synchronized(this){
+            boolean rc = false;
+            try {
+                FormBindingDefinitionREF val = typeCheck(v);
+                rc = value.containsValue(val);
+            } catch (DmcValueException e) {
+            }
+            return(rc);
+        }
+    }
+    
+    @Override
+    // org.dmd.dms.util.GenUtility.dumpMAPType(GenUtility.java:2650)
+    public boolean containsKey(Object key){
+        synchronized(this){
+            boolean rc = false;
+           if (key instanceof CamelCaseName)
+                rc = value.containsKey(key);
+            return(rc);
+        }
+    }
+    
+}
+
