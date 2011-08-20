6322a98dc2abd5b147bb282ff25ce96e94858f8f
pstrong99@gmail.com <pstrong99@gmail.com@180258a6-fd51-281c-9bda-22f77bc2175c>
2011-08-20 16:58:01 +0000

    Updated MethodWithArgs test case

--- trunk/ dark-matter-data/dark-matter-data/test/org/dmd/mvw/types/MethodWithArgsTest.java
+++ trunk/ dark-matter-data/dark-matter-data/test/org/dmd/mvw/types/MethodWithArgsTest.java
@@ -1,40 +1,46 @@
 package org.dmd.mvw.types;
 
-import static org.junit.Assert.assertTrue;
+import static org.junit.Assert.*;
 
 import org.dmd.dmc.DmcValueException;
-import org.dmd.mvw.tools.mvwgenerator.types.GetWithOptions;
+import org.dmd.mvw.tools.mvwgenerator.types.MethodWithArgs;
 import org.junit.Test;
 
 public class MethodWithArgsTest {
 
 	@Test
-	public void testStringConstructor(){
-		GetWithOptions gwm = null;
+	public void testOK1() throws DmcValueException{
+		MethodWithArgs mwa = null;
 		
-		try {
-			gwm = new GetWithOptions("contact");
-		} catch (DmcValueException e1) {
-			System.out.println(e1);
-			assertTrue("Unexpected exception", false);
-		}
+		mwa = new MethodWithArgs("void contactChanged(ContactDMO dmo) Called with the user hits Apply");
 		
-		try {
-			gwm = new GetWithOptions("contact MOOSE");
-			assertTrue("Should have indicated that the the option was illegal.", false);
-		} catch (DmcValueException e1) {
-			System.out.println(e1);
-			assertTrue("Expected exception", true);
-		}
+		assertEquals("Return type should be void","void",mwa.getReturnType());
 		
-		try {
-			gwm = new GetWithOptions("contact DMPERRORS RPCERRORS CENTRALERRORS CACHE CREATED DELETED MODIFIED");
-			System.out.println(gwm.toString());
-		} catch (DmcValueException e1) {
-			System.out.println(e1);
-			assertTrue("Unexpected exception", false);
-		}
+		assertEquals("Comment should be 'Called with the user hits Apply'","Called with the user hits Apply",mwa.getComment());
 		
+//		try {
+//			mwa = new MethodWithArgs("contact");
+//		} catch (DmcValueException e1) {
+//			System.out.println(e1);
+//			assertTrue("Unexpected exception", false);
+//		}
+//		
+//		try {
+//			gwm = new GetWithOptions("contact MOOSE");
+//			assertTrue("Should have indicated that the the option was illegal.", false);
+//		} catch (DmcValueException e1) {
+//			System.out.println(e1);
+//			assertTrue("Expected exception", true);
+//		}
+//		
+//		try {
+//			gwm = new GetWithOptions("contact DMPERRORS RPCERRORS CENTRALERRORS CACHE CREATED DELETED MODIFIED");
+//			System.out.println(gwm.toString());
+//		} catch (DmcValueException e1) {
+//			System.out.println(e1);
+//			assertTrue("Unexpected exception", false);
+//		}
+//		
 		
 
 	}
