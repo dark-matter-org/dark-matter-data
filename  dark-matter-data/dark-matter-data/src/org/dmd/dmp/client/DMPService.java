e72658d71f8deef52805fcdef980c5b3f560fad3
pstrong99 <pstrong99@gmail.com>
2011-08-21 23:43:43 +0000

    Added PreAuthRequest to DMP to retrieve info before login.

---  dark-matter-data/dark-matter-data/src/org/dmd/dmp/client/DMPService.java
+++  dark-matter-data/dark-matter-data/src/org/dmd/dmp/client/DMPService.java
@@ -14,6 +14,8 @@ import org.dmd.dmp.shared.generated.dmo.LoginRequestDMO;
 import org.dmd.dmp.shared.generated.dmo.LoginResponseDMO;
 import org.dmd.dmp.shared.generated.dmo.LogoutRequestDMO;
 import org.dmd.dmp.shared.generated.dmo.LogoutResponseDMO;
+import org.dmd.dmp.shared.generated.dmo.PreAuthRequestDMO;
+import org.dmd.dmp.shared.generated.dmo.PreAuthResponseDMO;
 import org.dmd.dmp.shared.generated.dmo.RequestDMO;
 import org.dmd.dmp.shared.generated.dmo.ResponseDMO;
 import org.dmd.dmp.shared.generated.dmo.SetRequestDMO;
@@ -25,29 +27,31 @@ import com.google.gwt.user.client.rpc.RemoteService;
 import com.google.gwt.user.client.rpc.RemoteServiceRelativePath;
 
 /**
- * The client side stub for the RPC service.
+ * The client side stub for the Dark Matter Protocol RPC service.
  */
 @RemoteServiceRelativePath("DMP")
 public interface DMPService extends RemoteService {
 	
-	  LoginResponseDMO 		login(LoginRequestDMO loginRequest);
+	PreAuthResponseDMO preauth(PreAuthRequestDMO request);
+
+	LoginResponseDMO login(LoginRequestDMO loginRequest);
 	  
-	  LogoutResponseDMO 	logout(LogoutRequestDMO logoutRequest);
+	LogoutResponseDMO logout(LogoutRequestDMO logoutRequest);
 	  
-	  GetResponseDMO		get(GetRequestDMO getRequest);
+	GetResponseDMO get(GetRequestDMO getRequest);
 
-	  SetResponseDMO		set(SetRequestDMO setRequest);
+	SetResponseDMO set(SetRequestDMO setRequest);
 
-	  CreateResponseDMO		create(CreateRequestDMO createRequest);
+	CreateResponseDMO create(CreateRequestDMO createRequest);
 
-	  DeleteResponseDMO		delete(DeleteRequestDMO deleteRequest);
+	DeleteResponseDMO delete(DeleteRequestDMO deleteRequest);
 
-	  ActionResponseDMO		action(ActionRequestDMO actionRequest);
+	ActionResponseDMO action(ActionRequestDMO actionRequest);
 
-	  NotifyResponseDMO		notify(NotifyRequestDMO notifyRequest);
+	NotifyResponseDMO notify(NotifyRequestDMO notifyRequest);
 
-	  DenotifyResponseDMO	denotify(DenotifyRequestDMO notifyRequest);
+	DenotifyResponseDMO	denotify(DenotifyRequestDMO notifyRequest);
 
-	  ResponseDMO			otherRequest(RequestDMO request);
+	ResponseDMO otherRequest(RequestDMO request);
   
 }
---  dark-matter-data/dark-matter-data/src/org/dmd/dmp/client/DMPServiceAsync.java
+++  dark-matter-data/dark-matter-data/src/org/dmd/dmp/client/DMPServiceAsync.java
@@ -14,6 +14,8 @@ import org.dmd.dmp.shared.generated.dmo.LoginRequestDMO;
 import org.dmd.dmp.shared.generated.dmo.LoginResponseDMO;
 import org.dmd.dmp.shared.generated.dmo.LogoutRequestDMO;
 import org.dmd.dmp.shared.generated.dmo.LogoutResponseDMO;
+import org.dmd.dmp.shared.generated.dmo.PreAuthRequestDMO;
+import org.dmd.dmp.shared.generated.dmo.PreAuthResponseDMO;
 import org.dmd.dmp.shared.generated.dmo.RequestDMO;
 import org.dmd.dmp.shared.generated.dmo.ResponseDMO;
 import org.dmd.dmp.shared.generated.dmo.SetRequestDMO;
@@ -27,25 +29,83 @@ import com.google.gwt.user.client.rpc.AsyncCallback;
  * The asynchronous counterpart of <code>DMPService</code>.
  */
 public interface DMPServiceAsync {
+	
+	/**
+	 * Allows for sending a request for data from the server before the user has authenticated.
+	 * This is useful for populating login screens with server specific info.
+	 * @param preathRequest The request.
+	 * @param callback The callback to handle the response.
+	 */
+	void preauth(PreAuthRequestDMO preathRequest, AsyncCallback<PreAuthResponseDMO> callback);
   
+	/**
+	 * Performs the login operation. 
+	 * @param loginRequest The request.
+	 * @param callback The callback to handle the response.
+	 */
 	void login(LoginRequestDMO loginRequest, AsyncCallback<LoginResponseDMO> callback);
 
+	/**
+	 * Performs the logout of a user.
+	 * @param logoutRequest The request.
+	 * @param callback The callback to handle the response.
+	 */
 	void logout(LogoutRequestDMO logoutRequest, AsyncCallback<LogoutResponseDMO> callback);
 
+	/**
+	 * Retrieves one or more objects from the server and may register for event notifications.
+	 * @param getRequest The request.
+	 * @param callback The callback to handle the response.
+	 */
 	void get(GetRequestDMO getRequest, AsyncCallback<GetResponseDMO> callback);
 
-	void set(SetRequestDMO getRequest, AsyncCallback<SetResponseDMO> callback);
+	/**
+	 * Alters an existing named object.
+	 * @param setRequest The request.
+	 * @param callback The callback to handle the response.
+	 */
+	void set(SetRequestDMO setRequest, AsyncCallback<SetResponseDMO> callback);
 
-	void create(CreateRequestDMO getRequest, AsyncCallback<CreateResponseDMO> callback);
+	/**
+	 * Creates a new object.
+	 * @param createRequest The request.
+	 * @param callback The callback to handle the response.
+	 */
+	void create(CreateRequestDMO createRequest, AsyncCallback<CreateResponseDMO> callback);
 
-	void delete(DeleteRequestDMO getRequest, AsyncCallback<DeleteResponseDMO> callback);
+	/**
+	 * Deletes an existing named object.
+	 * @param deleteRequest The request.
+	 * @param callback The callback to handle the response.
+	 */
+	void delete(DeleteRequestDMO deleteRequest, AsyncCallback<DeleteResponseDMO> callback);
 
+	/**
+	 * Performs some application specific action.
+	 * @param actionRequest The request.
+	 * @param callback The callback to handle the response.
+	 */
 	void action(ActionRequestDMO actionRequest, AsyncCallback<ActionResponseDMO> callback);
 
+	/**
+	 * Allows for registration of interest in events associated with objects.
+	 * @param notifyRequest The request.
+	 * @param callback The callback to handle the response.
+	 */
 	void notify(NotifyRequestDMO notifyRequest, AsyncCallback<NotifyResponseDMO> callback);
 
+	/**
+	 * Deregisters for event notification.
+	 * @param denotifyRequest The request.
+	 * @param callback The callback to handle the response.
+	 */
 	void denotify(DenotifyRequestDMO denotifyRequest, AsyncCallback<DenotifyResponseDMO> callback);
 
+	/**
+	 * Allows for the processing of application specific requests.
+	 * @param request The request.
+	 * @param callback The callback to handle the response.
+	 */
 	void otherRequest(RequestDMO request, AsyncCallback<ResponseDMO> callback);
 
 }
