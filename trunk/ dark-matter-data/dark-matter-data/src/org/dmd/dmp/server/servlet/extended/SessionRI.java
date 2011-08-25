06b61105065433c076a94fc82f306ffb60aa1d9f
pstrong99 <pstrong99@180258a6-fd51-281c-9bda-22f77bc2175c>
2011-08-25 18:59:33 +0000

    Small change to session to use handle to the event server rather than the servlet.

--- trunk/ dark-matter-data/dark-matter-data/src/org/dmd/dmp/server/servlet/extended/SessionRI.java
+++ trunk/ dark-matter-data/dark-matter-data/src/org/dmd/dmp/server/servlet/extended/SessionRI.java
@@ -1,14 +1,15 @@
 package org.dmd.dmp.server.servlet.extended;
 
-import de.novanic.eventservice.client.event.domain.Domain;
-import de.novanic.eventservice.client.event.domain.DomainFactory;
 import org.dmd.dmp.server.extended.DMPEvent;
 import org.dmd.dmp.server.extended.Request;
 import org.dmd.dmp.server.extended.Response;
 import org.dmd.dmp.server.servlet.base.interfaces.DmpEventHandlerIF;
 import org.dmd.dmp.server.servlet.base.interfaces.DmpResponseHandlerIF;
-import org.dmd.dmp.server.servlet.dmpservletri.DMPServiceImpl;
 import org.dmd.dmp.server.servlet.generated.dmw.SessionRIDMW;
+
+import de.novanic.eventservice.client.event.domain.Domain;
+import de.novanic.eventservice.client.event.domain.DomainFactory;
+import de.novanic.eventservice.service.RemoteEventServiceServlet;
 
 /**
  * The SessionRI class represents an authenticated user session. All requests
@@ -19,11 +20,11 @@ import org.dmd.dmp.server.servlet.generated.dmw.SessionRIDMW;
 public class SessionRI extends SessionRIDMW implements DmpResponseHandlerIF, DmpEventHandlerIF {
 	
 	// Our handle to the servlet which implements the GWT Event Service
-	DMPServiceImpl	servlet;
+	RemoteEventServiceServlet	servlet;
 	
 	Domain			domain;
 	
-	public void initializeEventChannel(DMPServiceImpl s){
+	public void initializeEventChannel(RemoteEventServiceServlet s){
 		servlet	= s;
 		domain 	= DomainFactory.getDomain(getSessionIDRI());
 	}
