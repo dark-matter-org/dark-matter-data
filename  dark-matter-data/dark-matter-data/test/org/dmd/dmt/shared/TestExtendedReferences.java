85bc1b8801b1d8db4c7758e94ed4e01a20f1473e
pstrong99 <pstrong99@180258a6-fd51-281c-9bda-22f77bc2175c>
2011-12-01 18:51:15 +0000

    Fix for NPE when applying an extended reference modifier to a DMO.

---  dark-matter-data/dark-matter-data/test/org/dmd/dmt/shared/TestExtendedReferences.java
+++  dark-matter-data/dark-matter-data/test/org/dmd/dmt/shared/TestExtendedReferences.java
@@ -2,11 +2,17 @@ package org.dmd.dmt.shared;
 
 
 import org.dmd.dmc.DmcValueException;
+import org.dmd.dmc.DmcValueExceptionSet;
 import org.dmd.dmc.types.StringName;
+import org.dmd.dmp.server.generated.DmpSchemaAG;
 import org.dmd.dmp.shared.generated.dmo.SetRequestDMO;
 import org.dmd.dms.generated.dmo.MetaDMSAG;
+import org.dmd.dmt.server.extended.ObjWithRefs;
+import org.dmd.dmt.server.generated.DmtSchemaAG;
 import org.dmd.dmt.shared.generated.dmo.ObjWithRefsDMO;
 import org.dmd.dmt.shared.generated.types.SomeRelation;
+import org.dmd.dmw.DmwOmni;
+
 import static org.junit.Assert.*;
 import org.junit.Test;
 
@@ -47,6 +53,29 @@ public class TestExtendedReferences {
         request.add(MetaDMSAG.__modify, modrec.getModifier());
         
         System.out.println(request.toOIF());
+	}
+	
+	@Test
+	public void testModification() throws DmcValueException, DmcValueExceptionSet{
+		DmpSchemaAG dmp = new DmpSchemaAG();
+		DmtSchemaAG dmt = new DmtSchemaAG();			
+		DmwOmni.instance().addSchema(dmp);
+		DmwOmni.instance().addSchema(dmt);
+
+		SomeRelation	rel = new SomeRelation(new StringName("some name"), 4, 2);
+
+		ObjWithRefs	owr1 = new ObjWithRefs();
+		owr1.setName(new StringName("obj1"));
+		
+		ObjWithRefs	owr2 = new ObjWithRefs();
+		owr2.setName(new StringName("obj2"));
+		
+		ObjWithRefs modrec = owr1.getModificationRecorder();
+		modrec.setNthSomeRelationMVI(0, rel);
+		
+		owr2.applyModifier(modrec.getModifier());
+		
+        System.out.println(owr2.toOIF());
 	}
 	
 }
