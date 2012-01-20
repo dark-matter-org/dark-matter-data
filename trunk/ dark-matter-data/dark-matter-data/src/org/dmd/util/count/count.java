629f811dae3d0ff17b5cbdfb8bee0f615c80a769
pstrong99 <pstrong99@180258a6-fd51-281c-9bda-22f77bc2175c>
2012-01-20 14:12:02 +0000

    Code code utility

---
+++ trunk/ dark-matter-data/dark-matter-data/src/org/dmd/util/count/count.java
@@ -0,0 +1,18 @@
+package org.dmd.util.count;
+
+import java.lang.*;
+import java.util.*;
+import java.io.*;
+
+public class count {
+
+    public static void main(String[] args) {
+        countMain c = new countMain();
+
+        c.run(args);
+
+        System.exit(0);
+    }
+
+}
+ 
