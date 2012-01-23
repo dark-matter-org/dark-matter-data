091a588a35c1de6f6adbe65afb8f342a0b8e74db
pstrong99 <pstrong99@180258a6-fd51-281c-9bda-22f77bc2175c>
2012-01-23 16:57:03 +0000

    Schema summary work.

---  dark-matter-data/dark-matter-data/src/org/dmd/dms/doc/web/Summarizer.java
+++  dark-matter-data/dark-matter-data/src/org/dmd/dms/doc/web/Summarizer.java
@@ -1,18 +1,52 @@
 package org.dmd.dms.doc.web;
 
+import java.util.Iterator;
 import java.util.TreeMap;
 
 import org.dmd.dmc.DmcValueExceptionSet;
 import org.dmd.dms.DmsDefinition;
+import org.dmd.dms.SchemaDefinition;
 import org.dmd.dms.SchemaManager;
 
 public class Summarizer {
 
+	TreeMap<Integer,SchemaDefinition>				allSchemasByID;
+	
+	TreeMap<String,SchemaDefinition>				allSchemasByName;
+
 	TreeMap<String,TreeMap<String,DmsDefinition>>	definitionsByLetter;
 	
-	public Summarizer(SchemaManager sm){
+	String 			outDir;
+	
+	// The content of the navigation sidebar
+	StringBuffer	sidebar;
+	
+	StringBuffer	idSummary;
+	
+	public Summarizer(SchemaManager sm, String od){
+		allSchemasByID 		= new TreeMap<Integer, SchemaDefinition>();
+		allSchemasByName 	= new TreeMap<String, SchemaDefinition>();
 		definitionsByLetter = new TreeMap<String, TreeMap<String,DmsDefinition>>();
+		outDir 				= od;
+		sidebar				= new StringBuffer();
+		idSummary			= new StringBuffer();
 		
+		Iterator<SchemaDefinition> sdit = sm.getSchemas();
+		if (sdit != null){
+			while(sdit.hasNext()){
+				SchemaDefinition sd = sdit.next();
+				allSchemasByID.put(sd.getSchemaBaseID(), sd);
+				allSchemasByName.put(sd.getName().getNameString(), sd);
+				
+			}
+		}
+		
+		buildSidebar();
+		
+		buildIDSummary();
+		
+		
+
 		for(DmsDefinition def: sm.allDefs.values()){
 			def.getDMO().clearReferenceInfo();
 		}
@@ -20,25 +54,58 @@ public class Summarizer {
 		for(DmsDefinition def: sm.allDefs.values()){
 			addDefinition(def);
 			try {
-//				if (def instanceof ClassDefinition){
-//					int temp = 5;
-//				}
-//				System.out.println(def.toOIF() + "\n\n");
-				
-//				if (def instanceof SchemaDefinition){
-//					if (def.getName().getNameString().equals("dmt"))
-//						DebugInfo.debug(def.toOIF());
-//					def.resolveReferences(sm);
-//				}
-//				else
-					def.resolveReferences(sm);
+				def.resolveReferences(sm);
 				
 			} catch (DmcValueExceptionSet e) {
 				// TODO Auto-generated catch block
 				e.printStackTrace();
 			}
 		}
+	}
+	
+	public String getSideBar(){
+		return(sidebar.toString());
+	}
+	
+	void buildSidebar(){
+		sidebar.append("    <div id=\"sidebar\">\n");
 		
+		for(SchemaDefinition sd: allSchemasByName.values()){
+			String name = sd.getName().getNameString();
+			sidebar.append("        <a class=\"navLink\" href=\"" + name + ".html\"> " + name + "</a>\n");
+		}
+		
+		sidebar.append("    </div>\n");
+	}
+	
+	void buildIDSummary(){
+		idSummary.append("<div class=\"idSummary\">\n\n");
+		
+		idSummary.append("<h2> Schema Identifier Ranges </h2>\n\n");
+		
+		idSummary.append("  <table>\n\n");
+		
+		for(SchemaDefinition sd: allSchemasByID.values()){
+			int end = sd.getSchemaBaseID() + sd.getSchemaIDRange();
+			String name = sd.getName().getNameString();
+			idSummary.append("    <tr>\n");
+			idSummary.append("    <td>\n");
+			idSummary.append("    </td>\n");
+			idSummary.append("    " + name + "\n");
+			idSummary.append("    <td>\n");
+			idSummary.append("    " + sd.getSchemaBaseID() + "\n");
+			idSummary.append("    </td>\n");
+			idSummary.append("    <td>\n");
+			idSummary.append("    " + end + "\n");
+			idSummary.append("    </td>\n");
+			idSummary.append("    </tr>\n");
+		}
+		
+		idSummary.append("  </table>\n\n");
+
+		idSummary.append("</div> <!-- idSummary -->\n\n");
+
+		idSummary.append("<table>\n");
 		
 	}
 	
