e7c30a13954bc8bc7faeacd0a843c80c3a44b472
pstrong99 <pstrong99@180258a6-fd51-281c-9bda-22f77bc2175c>
2012-06-06 15:39:04 +0000

    Removal of packages that have been split off to dark-matter-mvw and dark-matter-gxt

--- branches/1.1/src/org/dmd/mvw/tools/mvwgenerator/extended/forms/FormBindingDefinition.java
+++
@@ -1,7 +0,0 @@
-package org.dmd.mvw.tools.mvwgenerator.extended.forms;
-
-import org.dmd.mvw.tools.mvwgenerator.generated.dmw.FormBindingDefinitionDMW;
-
-public class FormBindingDefinition extends FormBindingDefinitionDMW {
-
-}
--- branches/1.1/src/org/dmd/mvw/tools/mvwgenerator/util/ComponentFormatter.java
+++
@@ -1,9 +0,0 @@
-package org.dmd.mvw.tools.mvwgenerator.util;
-
-/**
- * The ComponentFormatter provide common methods for formatting Controllers, Presenters and Activities.
- */
-public class ComponentFormatter {
-
-	
-}
