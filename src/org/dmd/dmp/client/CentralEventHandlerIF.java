70b11ef4172d8a8b69b878a42152e9d3038e9cfa
pstrong99 <pstrong99@180258a6-fd51-281c-9bda-22f77bc2175c>
2012-06-06 15:52:26 +0000

    Removal of packages that have been split off to dark-matter-mvw and dark-matter-gxt

--- src/org/dmd/dmp/client/CentralEventHandlerIF.java
+++
@@ -1,8 +0,0 @@
-package org.dmd.dmp.client;
-
-import org.dmd.dmp.shared.generated.dmo.DMPEventDMO;
-
-public interface CentralEventHandlerIF {
-
-	public void handleEventCentrally(DMPEventDMO event);
-}
