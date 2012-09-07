6bfbb516a76f1f56bc591f10fe4d859a2c68f7de
pstrong99 <pstrong99@180258a6-fd51-281c-9bda-22f77bc2175c>
2012-09-07 20:50:09 +0000

    Rule organization

---
+++ trunk/src/org/dmd/dmv/shared/AttributeValidatorCollection.java
@@ -0,0 +1,13 @@
+package org.dmd.dmv.shared;
+
+import org.dmd.dmc.rules.AttributeRuleCollection;
+import org.dmd.dms.shared.interfaces.AttributeValidationIF;
+
+public class AttributeValidatorCollection extends AttributeRuleCollection<AttributeValidationIF> {
+
+	public AttributeValidatorCollection(AttributeValidationIF rule) {
+		super(rule);
+		// TODO Auto-generated constructor stub
+	}
+
+}
