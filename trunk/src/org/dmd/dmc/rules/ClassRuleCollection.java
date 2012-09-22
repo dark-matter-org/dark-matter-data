f1d46dfa9cc49cc2b64f08e37283e476f829f493
pstrong99@gmail.com <pstrong99@gmail.com@180258a6-fd51-281c-9bda-22f77bc2175c>
2012-09-22 17:52:36 +0000

    Added RuleList initialization.

--- trunk/src/org/dmd/dmc/rules/ClassRuleCollection.java
+++ trunk/src/org/dmd/dmc/rules/ClassRuleCollection.java
@@ -30,10 +30,55 @@ public abstract class ClassRuleCollection<I extends RuleIF> extends RuleCollecti
 
 
 	protected ArrayList<I> getRules(DmcClassInfo ci){
-		ArrayList<I> rc  = null;
     	ClassRuleKey crk = new ClassRuleKey(ci);
+		RuleList<I> ruleList  = rules.get(crk);
+		
+		if (ruleList == null){
+			// We didn't have rules specifically for this class, so we create a
+			// place holder rule list and pass it in for initialization.
+			// The next we come through for the same key, we won't have to do the
+			// initialization again.
+			ruleList = new RuleList<I>();
+			initializeFromHere(crk, ruleList);
+		}
+		else{
+			// We have a rule list - if it hasn't yet been initialized, we initialize it
+			if (!ruleList.initialized())
+				initializeFromHere(crk, ruleList);
+		}
+		
 
-    	return(rc);
+    	return(ruleList.getRules());
+	}
+
+	/**
+	 * The first time we are asked to retrieve the rules for a particular class key, we ascend the
+	 * class hierarchy and gather all of the rules that would apply to the specified class and
+	 * all base classes as well. Once done, the rule list is marked as initialized and we don't bother
+	 * with this exercise again.
+	 * <p/>
+	 * The rule list that's passed in may in fact be empty, but we may find rules defined for the class
+	 * in the context of our base classes. Or, it may be that there are simply no rules for this class.
+	 * The generated derived class will check for global class rules that may have to be applied.
+	 * @param ark the starting rule key
+	 * @param ruleList the rule list for the specified key.
+	 */
+	private void initializeFromHere(ClassRuleKey crk, RuleList<I> ruleList){
+		DmcClassInfo base = crk.classInfo.derivedFrom;
+		
+		while(base != null){
+			ClassRuleKey baseKey = new ClassRuleKey(base);
+			RuleList<I> baseRules = rules.get(baseKey);
+			
+			if (baseRules != null){
+				ArrayList<I> rl = baseRules.getRules();
+				for(I rule: rl)
+					ruleList.addRule(rule);
+			}
+			base = base.derivedFrom;
+		}
+		
+		ruleList.setInitialized();
 	}
 
 }
