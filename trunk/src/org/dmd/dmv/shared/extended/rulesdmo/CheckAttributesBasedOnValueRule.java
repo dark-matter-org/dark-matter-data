fc2d912bb5dea66145732f6dbe20314547a97ee7
pstrong99 <pstrong99@180258a6-fd51-281c-9bda-22f77bc2175c>
2012-11-22 21:11:32 +0000

    More rule implementations.

--- trunk/src/org/dmd/dmv/shared/extended/rulesdmo/CheckAttributesBasedOnValueRule.java
+++ trunk/src/org/dmd/dmv/shared/extended/rulesdmo/CheckAttributesBasedOnValueRule.java
@@ -1,7 +1,14 @@
 package org.dmd.dmv.shared.extended.rulesdmo;
 
+import java.util.Iterator;
+
+import org.dmd.dmc.DmcAttribute;
+import org.dmd.dmc.DmcAttributeInfo;
 import org.dmd.dmc.DmcObject;
+import org.dmd.dmc.DmcOmni;
+import org.dmd.dmc.rules.DmcRuleException;
 import org.dmd.dmc.rules.DmcRuleExceptionSet;
+import org.dmd.dms.generated.types.AttributeDefinitionREF;
 import org.dmd.dmv.shared.generated.dmo.CheckAttributesBasedOnValueRuleDataDMO;
 import org.dmd.dmv.shared.generated.rulesdmo.CheckAttributesBasedOnValueRuleBaseImpl;
 
@@ -17,8 +24,41 @@ public class CheckAttributesBasedOnValueRule extends CheckAttributesBasedOnValue
 
 	@Override
 	public void execute(DmcObject obj) throws DmcRuleExceptionSet {
-		// TODO Auto-generated method stub
+		DmcRuleExceptionSet rc = null;
 		
+		DmcAttribute<?>	boa = obj.get(ruleDMO.getBasedOnAttribute().getObjectName().getNameString());
+		if (boa != null){
+			String value = boa.getSV().toString();
+			
+			if (ruleDMO.basedOnValueContains(value)){
+				if (ruleDMO.getExcludeThisAttributeSize() > 0){
+					Iterator<AttributeDefinitionREF> refs = ruleDMO.getExcludeThisAttributeREFs();
+					while(refs.hasNext()){
+						DmcAttributeInfo ai = DmcOmni.instance().getAttributeInfo(refs.next().getObjectName().getNameString());
+						if (obj.get(ai) != null){
+							if (rc == null)
+								rc = new DmcRuleExceptionSet();
+							rc.add(new DmcRuleException(ruleDMO.getRuleTitle() + "\nCan't have: " + ai.name, this));
+						}
+					}
+				}
+				if (ruleDMO.getIncludeThisAttributeSize() > 0){
+					Iterator<AttributeDefinitionREF> refs = ruleDMO.getIncludeThisAttributeREFs();
+					while(refs.hasNext()){
+						DmcAttributeInfo ai = DmcOmni.instance().getAttributeInfo(refs.next().getObjectName().getNameString());
+						if (obj.get(ai) == null){
+							if (rc == null)
+								rc = new DmcRuleExceptionSet();
+							rc.add(new DmcRuleException(ruleDMO.getRuleTitle() + "\nMust have: " + ai.name, this));
+						}
+					}
+					
+				}
+			}
+		}
+		
+		if (rc != null)
+			throw(rc);
 	}
 
 }
