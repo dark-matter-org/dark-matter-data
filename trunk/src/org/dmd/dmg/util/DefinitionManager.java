de36352b0c8ad5e36295ef149ce5c52608ae7990
pstrong99@gmail.com <pstrong99@gmail.com@180258a6-fd51-281c-9bda-22f77bc2175c>
2013-03-11 00:32:52 +0000

    Adding new base definition manager. Fixed issue with GenUtility to include DmcAttribute if an object is named.

---
+++ trunk/src/org/dmd/dmg/util/DefinitionManager.java
@@ -0,0 +1,15 @@
+package org.dmd.dmg.util;
+
+/**
+ * The DefinitionManager is a generic manager of a set of objects that comprise definitions
+ * for some purpose.
+ */
+public class DefinitionManager {
+	
+	
+	
+	public DefinitionManager(){
+		
+	}
+
+}
