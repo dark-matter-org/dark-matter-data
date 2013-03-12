8427ac15bb442642680463192a8831675a75705a
pstrong99 <pstrong99@gmail.com>
2013-03-12 03:20:40 +0000

    Generic definition manager

--- src/org/dmd/dmg/util/DefinitionManager.java
+++ src/org/dmd/dmg/util/DefinitionManager.java
@@ -1,14 +1,28 @@
 package org.dmd.dmg.util;
 
+import java.util.ArrayList;
+import java.util.TreeMap;
+
+import org.dmd.dmc.DmcHierarchicObjectName;
+import org.dmd.dmc.DmcObject;
+import org.dmd.dmc.DmcObjectName;
+
 /**
  * The DefinitionManager is a generic manager of a set of objects that comprise definitions
  * for some purpose.
  */
-public class DefinitionManager {
+public class DefinitionManager<DEF extends DmcObject, FLATNAME extends DmcObjectName, FQN extends DmcHierarchicObjectName> {
 	
+	TreeMap<FLATNAME,ArrayList<DEF>>	flatMap;
 	
+	TreeMap<FQN,DEF>					fqnMap;
 	
 	public DefinitionManager(){
+		flatMap	= new TreeMap<FLATNAME, ArrayList<DEF>>();
+		fqnMap	= new TreeMap<FQN, DEF>();
+	}
+	
+	public void addDefinition(){
 		
 	}
 
