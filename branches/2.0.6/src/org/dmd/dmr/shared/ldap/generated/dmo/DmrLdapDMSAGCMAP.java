39dfe0a7a8fbaa1967984c0b881df6813c970801
pstrong99@gmail.com <pstrong99@gmail.com@180258a6-fd51-281c-9bda-22f77bc2175c>
2013-07-14 12:30:25 +0000

    Breaking down the static initialization of the compact schema into separate functions didn't have the desired outcome; the Java compiler recombines static initializations into a single function. The new approach is to generate separate helper classes schemaAMAPDMSAG and schemaCMAPDMSAG that have a separate static method that performs initialization of the attribute and class maps. Ideally, schemas shouldn't grow so large that this becomes an issue, but this should prevent the static initializers exceeding length limits.

---
+++ branches/2.0.6/src/org/dmd/dmr/shared/ldap/generated/dmo/DmrLdapDMSAGCMAP.java
@@ -0,0 +1,20 @@
+package org.dmd.dmr.shared.ldap.generated.dmo;
+
+// Generated from: org.dmd.util.codegen.ImportManager.getFormattedImports(ImportManager.java:82)
+// Called from: org.dmd.dms.util.DmoCompactSchemaFormatter.dumpClassMapInitializer(DmoCompactSchemaFormatter.java:452)
+import java.util.HashMap;         // For storage of schema info - (DmoCompactSchemaFormatter.java:449)
+import org.dmd.dmc.*;             // Basic DMC stuff - (DmoCompactSchemaFormatter.java:450)
+
+
+// Generated from: org.dmd.dms.util.DmoCompactSchemaFormatter.dumpClassMapInitializer(DmoCompactSchemaFormatter.java:456)
+public class DmrLdapDMSAGCMAP {
+
+    static protected void initCmAp(HashMap<Integer, DmcClassInfo> _CmAp){
+        _CmAp.put(DmrLdapDMSAG.__LDAPAttributeAUX.id,DmrLdapDMSAG.__LDAPAttributeAUX);
+        _CmAp.put(DmrLdapDMSAG.__LDAPClassAUX.id,DmrLdapDMSAG.__LDAPClassAUX);
+        _CmAp.put(DmrLdapDMSAG.__LDAPHierarchicObject.id,DmrLdapDMSAG.__LDAPHierarchicObject);
+        _CmAp.put(DmrLdapDMSAG.__LDAPSchemaAUX.id,DmrLdapDMSAG.__LDAPSchemaAUX);
+    }
+
+}
+
