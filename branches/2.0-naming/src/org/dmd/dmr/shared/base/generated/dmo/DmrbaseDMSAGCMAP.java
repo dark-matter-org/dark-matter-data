21e2db12644fb14a854f8606c4ccc7a2163eb6dd
pstrong99 <pstrong99@180258a6-fd51-281c-9bda-22f77bc2175c>
2013-10-01 15:02:24 +0000

    Manual porting of the following change from 2.0.6 r1393
    Breaking down the static initialization of the compact schema into separate
    functions didn't have the desired outcome; the Java compiler recombines static
    initializations into a single function. The new approach is to generate separate
    helper classes schemaAMAPDMSAG and schemaCMAPDMSAG that have a separate static
    method that performs initialization of the attribute and class maps. Ideally,
    schemas shouldn't grow so large that this becomes an issue, but this should
    prevent the static initializers exceeding length limits.

---
+++ branches/2.0-naming/src/org/dmd/dmr/shared/base/generated/dmo/DmrbaseDMSAGCMAP.java
@@ -0,0 +1,18 @@
+package org.dmd.dmr.shared.base.generated.dmo;
+
+// Generated from: org.dmd.util.codegen.ImportManager.getFormattedImports(ImportManager.java:82)
+// Called from: org.dmd.dms.util.DmoCompactSchemaFormatter.dumpClassMapInitializer(DmoCompactSchemaFormatter.java:483)
+import java.util.HashMap;         // For storage of schema info - (DmoCompactSchemaFormatter.java:480)
+import org.dmd.dmc.*;             // Basic DMC stuff - (DmoCompactSchemaFormatter.java:481)
+
+
+// Generated from: org.dmd.dms.util.DmoCompactSchemaFormatter.dumpClassMapInitializer(DmoCompactSchemaFormatter.java:487)
+public class DmrbaseDMSAGCMAP {
+
+    static protected void initCmAp(HashMap<Integer, DmcClassInfo> _CmAp){
+        _CmAp.put(DmrbaseDMSAG.__DotNamedObject.id,DmrbaseDMSAG.__DotNamedObject);
+        _CmAp.put(DmrbaseDMSAG.__HierarchicObject.id,DmrbaseDMSAG.__HierarchicObject);
+    }
+
+}
+
